453e58173cb383fa508d8a03c57b7d848de3095b
anand880441-source <anand.suthar.cg@gmail.com>
2025-09-03 09:48:38 +0530

    ERROR EDIT IN Q10

--- ANAND SUTHAR/C/Q10.c
+++ ANAND SUTHAR/C/Q10.c
@@ -4,7 +4,7 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    if (n & (n - 1) == 0) {
+    if ((n & (n - 1)) == 0) {
         printf("%d is a power of 2.\n", n);
     } else{
         printf("%d is not a power of 2.\n", n);
